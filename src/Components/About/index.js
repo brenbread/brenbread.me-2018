b7055ea06bf27a8fbc9028eec4d2b6ce6e8efe1c
Bren Santos <santos.brenj@gmail.com>
2018-10-31 22:47:24 -0700

    Added photo of myself to the About Me section

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -11,19 +11,28 @@ class About extends Component {
       )
     }
     return (
+      <div>
       <Row>
         <Col className="text-left" sm="12" md={{ size: 6, offset: 3 }}>
           <h1>About Me</h1>
           <Card body inverse style={{ backgroundColor: 'transparent', borderColor: 'transparent' }}>
-              <CardText>
-                Hey! I'm a Computer Engineering major @<a href="https://cmpe.sjsu.edu/" target="_blank" rel="noopener noreferrer">SJSU</a> who enjoys software and hardware development.
-              </CardText>
-              <CardText>
-                I currently work as a web developer for <a href="https://sce.engr.sjsu.edu/" target="_blank" rel="noopener noreferrer">SJSU's Software & Computer Engineering Society</a> developing the new homepage of the website using React.
-              </CardText>
-              <CardText>
-                I'm also currently working as a web developer at SJSU contributing to <a href="https://github.com/kammce/Telemetry" target="_blank" rel="noopener noreferrer">Telemetry</a>, a web application that interacts with serial devices such as the SJOne/SJTwo board, Arduino, and FPGA devices.
-              </CardText>
+              <Row>
+                 <Col>
+                   <CardText>
+                     Hey! I'm a Computer Engineering major @<a href="https://cmpe.sjsu.edu/" target="_blank" rel="noopener noreferrer">SJSU</a> who enjoys software and hardware development.
+                   </CardText>
+                   <CardText>
+                     I currently work as a web developer for <a href="https://sce.engr.sjsu.edu/" target="_blank" rel="noopener noreferrer">SJSU's Software & Computer Engineering Society</a> developing the new homepage of the website using React.
+                   </CardText>
+                   <CardText>
+                     I'm also currently working as a web developer at SJSU contributing to <a href="https://github.com/kammce/Telemetry" target="_blank" rel="noopener noreferrer">Telemetry</a>, a web application that interacts with serial devices such as the SJOne/SJTwo board, Arduino, and FPGA devices.
+                   </CardText>
+                 </Col>
+                 <Col>
+                   <img src={require('../../assets/img/bren.jpeg')} />
+                 </Col>
+              </Row>
+              <br />
               <CardText>
                 Aside from that, I'm a partnered streamer on <a href="https://twitch.tv/brenbread" target="_blank" rel="noopener noreferrer">Twitch</a> and, nowadays, I stream on a very rare basis. I also enjoy listening to electronic music, playing rhythm games (like Sound Voltex and Dance Dance Revolution), <a href="https://soundcloud.com/brenbread" target="_blank" rel="noopener noreferrer">music production</a>, and watching some anime.
               </CardText>
@@ -53,6 +62,7 @@ class About extends Component {
             </Card>
         </Col>
       </Row>
+    </div>
     );
   }
 }

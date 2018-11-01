b5a9c4684743988f21c099cfb58d8d5a0ec22582
Bren Santos <santos.brenj@gmail.com>
2018-10-31 23:39:20 -0700

    Fixed navbar links

--- src/Components/NavBar/index.js
+++ src/Components/NavBar/index.js
@@ -54,16 +54,16 @@ class NavBar extends Component {
           </DropdownToggle>
           <DropdownMenu className="drop-menu" right>
             <DropdownItem className="item-1">
-              <NavLink href="/sdvx">
+              <NavLink href="/sdvx" target="_blank" rel="noopener noreferrer">
                 <Badge color="info">WIP</Badge>&nbsp;SDVX Calculators</NavLink>
             </DropdownItem>
             <DropdownItem divider className="drop-divider"/>
             <DropdownItem className="item-2">
-              <NavLink href="/score-calculator">
+              <NavLink href="/score-calculator" target="_blank" rel="noopener noreferrer">
                 <Badge color="secondary">OLD</Badge>&nbsp;SDVX Score Calculator</NavLink>
             </DropdownItem>
             <DropdownItem className="item-3">
-              <NavLink href="/score-calculator">
+              <NavLink href="/grade-calculator" target="_blank" rel="noopener noreferrer">
                 <Badge color="secondary">OLD</Badge>&nbsp;SDVX Grade Calculator</NavLink>
             </DropdownItem>
           </DropdownMenu>
